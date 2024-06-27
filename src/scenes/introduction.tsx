1e459a9e4a6f60a570dcf1344eb93f78ab18c182
Damian Sire <damiansire@gmail.com>
2024-06-27 02:12:19 -0300

    template: set expert image from background

--- src/scenes/introduction.tsx
+++ src/scenes/introduction.tsx
@@ -1,34 +1,27 @@
-import { makeScene2D, Code, lines, Circle, Line, Txt } from "@motion-canvas/2d";
+import { makeScene2D, Img, View2D, Circle } from "@motion-canvas/2d";
 import {
+  Color,
+  Reference,
   beginSlide,
   createRef,
-  createSignal,
-  DEFAULT,
-  Vector2,
   waitFor,
 } from "@motion-canvas/core";
+import sliderTemplate1 from "../../images/template/slider1.png";
+import { SetBackground } from "../libs/layout";
 
 export default makeScene2D(function* (view) {
-  const code = createRef<Code>();
-  view.fill("#000000");
-  view.add(
-    <Code
-      ref={code}
-      fontSize={28}
-      offsetX={-1}
-      x={-400}
-      code={`\
-function hello() {
-  console.log('Hello');
-}`}
-    />
-  );
+  const backgroundImg = createRef<Img>();
+  view.add(<Img src={sliderTemplate1} ref={backgroundImg} />);
+  SetBackground(backgroundImg, view);
 
-    // select all instances of "hello" (case-insensitive)
-  yield* code().selection(code().findAllRanges(/hello/gi), 1);
+  const myCircle = new Circle({
+    size: 300, // Adjust the radius as needed
+    stroke: new Color("#a7a7a7"), // Set the stroke color to red (or any color you want)
+    lineWidth: 5, // Adjust the line width as needed
+  });
+
+  view.add(myCircle);
+
   yield* waitFor(1);
   yield* beginSlide("first slide");
-
-
-
 });
