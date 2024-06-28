7fe56189a07558c1a8041c00310d13a69d2bee79
Damian Sire <damiansire@gmail.com>
2024-06-28 02:02:17 -0300

    assets: add new assets

--- src/assets.ts
+++ src/assets.ts
@@ -4,9 +4,11 @@ import angularLogoSvg from "../images/icons/angular.svg";
 import instagramIcon from "../images/icons/instagram.svg";
 import linkedinIcon from "../images/icons/linkedin.svg";
 import twitterIcon from "../images/icons/twitter.svg";
+import youtubeIcon from "../images/icons/youtube.png";
 
 //Templates
 import sliderTemplate1 from "../images/template/slider1.png";
+import sliderTemplate2 from "../images/template/slider2.png";
 
 //General
 import meExpertImg from "../images/me-expert.png";
@@ -17,6 +19,8 @@ export {
   instagramIcon,
   linkedinIcon,
   twitterIcon,
+  youtubeIcon,
   meExpertImg,
   sliderTemplate1,
+  sliderTemplate2,
 };
